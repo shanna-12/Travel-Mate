5a1ff4b98d804991ec462ffd86b4053151de8874
Kris Lefebure <krislefebure@hotmail.com>
2026-03-10 18:03:05 +0100

    MessagesController

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,9 @@
 class MessagesController < ApplicationController
-  SYSTEM_PROMPT = "You are a Teaching Assistant.\n\nI am a student at the Le Wagon AI Software Development Bootcamp, learning how to code.\n\nHelp me break down my problem into small, actionable steps, without giving away solutions.\n\nAnswer concisely in Markdown."
+  SYSTEM_PROMPT = "You are a Travel Assistant.\n\nI am looking for travel advise.\n\nHelp me find a holiday itinerary that suits my holiday type.\n\nAnswer concisely in Markdown."
 
   def create
     @chat = current_user.chats.find(params[:chat_id])
-    @challenge = @chat.challenge
+    @itinerary = @chat.itinerary
 
     @message = Message.new(message_params)
     @message.chat = @chat
@@ -11,9 +11,9 @@ class MessagesController < ApplicationController
 
     if @message.save
       ruby_llm_chat = RubyLLM.chat
-      response = ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
+      response = ruby_llm_chat.with_instructions(instructions).ask(@message.content)
       Message.create(role: "assistant", content: response.content, chat: @chat)
-
+      @chat.generate_title_from_first_message # NEW
       redirect_to chat_path(@chat)
     else
       render "chats/show", status: :unprocessable_entity
@@ -22,11 +22,15 @@ class MessagesController < ApplicationController
 
   private
 
-  def challenge_context
-    "Here is the context of the challenge: #{@challenge.content}."
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def itinerary_context
+    "Here is the context of the itinerary: #{@itinerary.holiday_type} #{@itinerary.start_date}."
   end
 
   def instructions
-    [SYSTEM_PROMPT, challenge_context].compact.join("\n\n")
+    [SYSTEM_PROMPT, itinerary_context].compact.join("\n\n")
   end
 end
