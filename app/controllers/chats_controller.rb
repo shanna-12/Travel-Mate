65b57f6b78fd88237ccffad50bdfed109a2d1e79
Prasanna Sivabalan <spshanna2010@gmail.com>
2026-03-10 18:14:26 +0100

    chat controller finished

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,20 @@
 class ChatsController < ApplicationController
+  def create
+    @itinerary = Itinerary.find(params[:itinerary_id])
+    @chat = Chat.new(title: "Untitled")
+    @chat.challenge = @challenge
+    @chat.user = current_user
+
+    if @chat.save
+      redirect_to chat_path(@chat)
+    else
+      @chats = @itinerary.chats.where(user: current_user)
+      render "itineraries/show"
+    end
+  end
+
+  def show
+    @chat = current_user.chats.find(params[:id])
+    @message = Message.new
+  end
 end
