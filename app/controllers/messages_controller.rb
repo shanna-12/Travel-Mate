c21dcf751c7600faf0de50d77aebf445e33d8caa
Kris Lefebure <krislefebure@hotmail.com>
2026-03-11 14:16:59 +0100

    made correction to create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,8 +13,7 @@ class MessagesController < ApplicationController
       ruby_llm_chat = RubyLLM.chat
       build_conversation_history # now it remembers it!
       response = ruby_llm_chat.with_instructions(instructions).ask(@message.content)
-      Message.create(role: "assistant", content: response.content, chat: @chat)
-      # @chat.messages.create(role: "assistant", content: response.content)
+      @chat.messages.create(role: "assistant", content: response.content)
       @chat.generate_title_from_first_message # NEW
       redirect_to chat_path(@chat)
     else
@@ -31,12 +30,12 @@ class MessagesController < ApplicationController
   def itinerary_context
     "Here is the context of the itinerary:
     #{@itinerary.holiday_type.present? ? "a " + @itinerary.holiday_type + " holiday" : ""}
-    #{@itinerary.destination.present? ? ", to destination " @itinerary.destination : ""}
+    #{@itinerary.destination.present? ? ", to destination " + @itinerary.destination : ""}
     #{@itinerary.start_date.present? ? ", from " + @itinerary.start_date : ""}
     #{@itinerary.end_date.present? ? " to " + @itinerary.end_date : ""}
     #{@itinerary.adults.present? ? ", with " + @itinerary.adults + " adults" : ""}
     #{@itinerary.children.present? ? " and " + @itinerary.children + " children" : ""}
-    #{@itinerary.budget.present? ? ", my buget is " + @itinerary.budget + " €" : ""}."
+    #{@itinerary.budget.present? ? ", my budget is " + @itinerary.budget + " €" : ""}."
   end
 
   def instructions
