86665e0f4a390bed1ab227b83cc3d8dadf15b755
Kris Lefebure <krislefebure@hotmail.com>
2026-03-11 12:40:25 +0100

    added history with limit

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,15 @@
 class Message < ApplicationRecord
   belongs_to :chat
+
+  MAX_USER_MESSAGES = 10
+
+  validate :user_message_limit, if: -> { role == "user" }
+
+  private
+
+  def user_message_limit
+    if chat.messages.where(role: "user").count >= MAX_USER_MESSAGES
+      errors.add(:content, "You can only send #{MAX_USER_MESSAGES} messages per chat.")
+    end
+  end
 end
