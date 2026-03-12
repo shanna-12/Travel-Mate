448209c7ca645d0972acd890e506804d8ef85395
@users.noreply.github.com <shwetha.putaleekar@gmail.com>
2026-03-12 17:41:50 +0100

    date conflict issue resolved

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -4,4 +4,13 @@ class Itinerary < ApplicationRecord
   end
   belongs_to :user
   has_many :chats, dependent: :destroy
+
+  validate :end_date_after_start_date
+
+  def end_date_after_start_date
+    return if start_date.blank? || end_date.blank?
+    return if end_date >= start_date
+
+    errors.add(:end_date, "must be on or after the start date")
+  end
 end
